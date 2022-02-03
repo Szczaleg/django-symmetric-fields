12fed48593ef0a868e11bb78999e2df442f6b4e7
Szczaleg <igor.strzalka@gmail.com>
2022-02-03 10:43:59 +0100

    Remove unused files, add requirements

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,7 @@
-from encrypted.settings import *
 
 INSTALLED_APPS = ('tests', )
+
+ENCRYPTION_KEYS = [b'qoVQ-4GEd4NO9bBKB6RKvGrzQ34-BiloN-UzV65jcko=', b'20q_FiBe6EF9lcEzupYqz5WeMUW_SULk6RRwvjEzhtI=']
 
 DATABASES = {
     "default": {
